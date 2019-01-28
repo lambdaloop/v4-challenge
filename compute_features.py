f5604a92db936c8d4ee45c854dc47b6bb2548f7f
abigelow1 <37598336+abigelow1@users.noreply.github.com>
2019-01-27 20:10:23 -0800

    Update compute_features.py

--- compute_features.py
+++ compute_features.py
@@ -1,6 +1,19 @@
 #!/usr/bin/env python3
 
 import numpy as np
+from scipy.stats import kurtosis, skew
 
 def raw_pixels(image):
     return image
+
+def mean_pixels(image): #mean pixel value of image
+    return np.mean(image)
+
+def std_pixels(image): #std of pixel values
+    return np.std(image)
+
+def kurt(image):
+    return kurtosis(image,axis=None)
+
+def skw(image):
+    return skew(image,axis=None)
