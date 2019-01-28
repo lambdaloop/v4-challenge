5cd86e9b9012c2042ddae7965e35756e0791d569
abigelow1 <37598336+abigelow1@users.noreply.github.com>
2019-01-27 20:11:28 -0800

    Update compute_features.py

--- compute_features.py
+++ compute_features.py
@@ -7,13 +7,13 @@ def raw_pixels(image):
     return image
 
 def mean_pixels(image): #mean pixel value of image
-    return np.mean(image)
+    return [np.mean(image)]
 
 def std_pixels(image): #std of pixel values
-    return np.std(image)
+    return [np.std(image)]
 
 def kurt(image):
-    return kurtosis(image,axis=None)
+    return [kurtosis(image,axis=None)]
 
 def skw(image):
-    return skew(image,axis=None)
+    return [skew(image,axis=None)]
