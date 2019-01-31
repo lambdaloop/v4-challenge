db06640609f1beebd786fe170a5964d3ad590189
Pierre Karashchuk <krchtchk@gmail.com>
2019-01-31 00:38:12 -0800

    iteration on the model fitting

--- fit_models.py
+++ fit_models.py
@@ -2,39 +2,54 @@
 
 import numpy as np
 from sklearn.linear_model import Lasso, Ridge, ElasticNet
-from sklearn.model_selection import train_test_split, cross_val_score
+from sklearn.model_selection import train_test_split, cross_val_score, ShuffleSplit
 from sklearn.metrics import r2_score
 from sklearn.metrics.scorer import make_scorer
 from sklearn.ensemble import RandomForestRegressor, ExtraTreesRegressor
+from sklearn.decomposition import PCA
 import pandas as pd
+
 
 train = pd.read_csv('data/train.csv')
 
-dd = np.load('data/features.npz')
-features = dict()
-for k in dd.keys():
-    features[k] = dd[k]
+# dd = np.load('data/features.npz')
+# features = dict()
+# for k in dd.keys():
+#     features[k] = dd[k]
 
-models = [Lasso(alpha=10), Ridge(alpha=1000), ElasticNet(), RandomForestRegressor(max_depth=5), ExtraTreesRegressor()]
+models = [Lasso(alpha=1000000), Ridge(alpha=100), ElasticNet(), RandomForestRegressor(max_depth=7), ExtraTreesRegressor()]
 
+# things to iterate over
 model = models[1]
-feature = 'stats'
+feature = 'gabor'
+neuron_number = 0
 
 ids = np.array(train['Id'])
 
-X_full = features[feature][ids]
-y_full = np.array(train.iloc[:, 1])
+# X_full = features[feature][ids]
+X_full = np.hstack([features['stats'][ids], features[feature][ids]])
+y_full = np.array(train.iloc[:, neuron_number])
+
+good = ~np.isnan(y_full)
+X_full = X_full[good]
+y_full = y_full[good]
 
 good = np.std(X_full, axis=0) > 0
 X_full = X_full[:, good]
 
-scores = cross_val_score(model, X_full, y_full, cv=3, scoring=make_scorer(r2_score))
+pca = PCA(n_components=min(X_full.shape[1], 10))
+X_full = pca.fit_transform(X_full)
 
-X_train, X_test, y_train, y_test = train_test_split(
-    X_full, y_full, test_size=0.2)
+scores = cross_val_score(model, X_full, y_full,
+                         cv=ShuffleSplit(n_splits=3, test_size=0.1, random_state=42),
+                         scoring=make_scorer(r2_score))
+print(np.mean(scores))
 
-model.fit(X_train, y_train)
-y_pred = model.predict(X_test)
-r2 = r2_score(y_test, y_pred)
+# X_train, X_test, y_train, y_test = train_test_split(
+#     X_full, y_full, test_size=0.1)
 
-print(r2)
+# model.fit(X_train, y_train)
+# y_pred = model.predict(X_test)
+# r2 = r2_score(y_test, y_pred)
+
+# print(r2)
