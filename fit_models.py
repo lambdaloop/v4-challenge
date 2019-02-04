b1a4219240f1cd9b25693414b517476f6005bdf4
abigelow1 <37598336+abigelow1@users.noreply.github.com>
2019-02-04 11:07:39 -0800

    Update fit_models.py

--- fit_models.py
+++ fit_models.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python3
-import warnings
 
 import numpy as np
 from sklearn.linear_model import Lasso, Ridge, ElasticNet
@@ -9,10 +8,7 @@ from sklearn.metrics.scorer import make_scorer
 from sklearn.ensemble import RandomForestRegressor, ExtraTreesRegressor
 from sklearn.decomposition import PCA
 import pandas as pd
-from bayes_opt import BayesianOptimization
 from tqdm import tqdm, trange
-
-warnings.simplefilter(action='ignore',category=FutureWarning)
 
 train = pd.read_csv('data/train.csv')
 test = pd.read_csv('data/sub.csv')
@@ -23,26 +19,10 @@ for k in dd.keys():
     features[k] = dd[k]
 
 models = [Lasso(alpha=1000000), Ridge(alpha=100000), ElasticNet(),
-          RandomForestRegressor(max_depth=7, n_estimators=10),
-          ExtraTreesRegressor(max_depth=7, n_estimators=10)]
-
-all_params = [{'alpha': (0.01, 1e6)}, {'alpha': (0.01, 1e6)}, {'alpha': (0.01, 1e6)},
-              {'max_depth': (3, 15)}, {'max_depth': (3, 15)}]
-
+          RandomForestRegressor(max_depth=7), ExtraTreesRegressor()]
 
 print(features.keys())
 # ['raw', 'LAB', 'fourier', 'gabor', 'stats']
-
-def train_models_fun(model, X_full, y_full):
-    def test_model(**params):
-        model.set_params(**params)
-        scores = cross_val_score(model, X_full, y_full,
-                                 cv=ShuffleSplit(n_splits=1, test_size=0.1, random_state=42),
-                                 scoring=make_scorer(r2_score))
-        r2_test = np.mean(scores)
-        return r2_test
-    return test_model
-
 
 for neuron_number in trange(1, train.shape[1], ncols=80):
     # print('neuron number:', neuron_number)
@@ -51,7 +31,6 @@ for neuron_number in trange(1, train.shape[1], ncols=80):
     for modelnum in trange(len(models), ncols=80):
         # print('model num:', modelnum)
         model = models[modelnum]
-        model_params = all_params[modelnum]
 
         for feature in tqdm(features.keys(), ncols=80):
 
@@ -78,13 +57,11 @@ for neuron_number in trange(1, train.shape[1], ncols=80):
             X_full = pca.transform(X_full)
             X_test = pca.transform(X_test)
 
-            fun = train_models_fun(model, X_full, y_full)
-            net_opt = BayesianOptimization(fun,model_params,verbose=False)
-            net_opt.maximize(init_points=5,n_iter=25,acq="ei",xi=1e-1)
+            scores = cross_val_score(model, X_full, y_full,
+                                     cv=ShuffleSplit(n_splits=3, test_size=0.1, random_state=42),
+                                     scoring=make_scorer(r2_score))
+            r2_test = np.mean(scores)
 
-            r2_test = net_opt.max['target']
-            best_params = net_opt.max['params']
-            model.set_params(**best_params)
             # print('feature: {}, r2: {:.3f}'.format(feature, r2_test))
 
             if r2_test > best_r2:
